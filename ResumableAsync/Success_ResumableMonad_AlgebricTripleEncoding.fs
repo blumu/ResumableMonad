96c9b478159f52ae8d27fb6268d9e249b6eb2740
William Blum <wiblum@microsoft.com>
2015-10-11 17:10:40 -0700

    getZero through boxing (casting working superficially)

--- ResumableAsync/Success_ResumableMonad_AlgebricTripleEncoding.fs
+++ ResumableAsync/Success_ResumableMonad_AlgebricTripleEncoding.fs
@@ -8,22 +8,26 @@ type ITriple<'u,'a,'v> =
 
 type FakeUnit = FakeUnit
 
-// type X<'t> = 
-//     abstract member Zero : unit -> 't
+type IZero = //<'t> = 
+     abstract member Zero : obj //'t
 
 [<StructuredFormatDisplay("ε")>]
 type Epsilon() =
-    //interface X<FakeUnit> with
-    //    member __.Zero () = FakeUnit
+    interface IZero with // <FakeUnit> with
+        member __.Zero = box FakeUnit
     //interface ITriple<FakeUnit,Cell<FakeUnit>,FakeUnit> with
     //    member __.U = FakeUnit
     //    member __.A = NotExecuted
     //    member __.V = FakeUnit
     override __.ToString() = "ε"
 
-and Cell<'t> = 
+
+type Cell<'t> = 
     | NotExecuted
     | Result of 't
+
+    interface IZero with //<Cell<'t>> with
+        member __.Zero = box NotExecuted
 
     interface ITriple<Epsilon,Cell<'t>,Epsilon> with
         member __.U = Epsilon()
@@ -33,28 +37,118 @@ and Cell<'t> =
 let epsilon = Epsilon()
 
 
+
 type Resumable<'h,'t> =
     {
         resume : 'h -> 'h * Cell<'t>
         //zero_previous : 'h
         //zero_last : 't
+        zero : IZero
     }
 
-
 //[<StructuredFormatDisplay("[{U},{A},{V}]")>]
-type Triple<'u,'a,'v>(u:'u,a:Cell<'a>,v:'v) =
+type Triple<'u,'a,'v
+                //when 'u :> IZero
+                // and 'v :> IZero
+                  > (u:'u,a:Cell<'a>,v:'v) =
 
-    member __.IAmATriple () = ()
     interface ITriple<'u,Cell<'a>,'v> with
         member x.U = u
         member x.A = a
         member x.V = v
-    //interface X<'a> with
-    //    member __.Zero () = Unchecked.defaultof<'a>
+    //interface IZero with
+    //    member __.Zero = 
+    //        let x = unbox<IZero> u.Zero
+    //        let y = unbox<IZero> v.Zero
+    //        box <| Triple<_, Cell<'a>, _ >
+    //                        (x, NotExecuted, y)
+
     override x.ToString() =
         sprintf "%A (%A) %O" u a v
 
 // Triple(epsilon,Result 5,epsilon)
+
+open Microsoft.FSharp.Reflection
+
+let rec getZero (_type:System.Type) =
+    match _type.Name with
+    | "Epsilon" -> box epsilon
+    | "Cell`1" -> box NotExecuted
+    | "Triple`3" ->
+        let tu = _type.GenericTypeArguments.[0]
+        let ta = _type.GenericTypeArguments.[1]
+        let tv = _type.GenericTypeArguments.[2]
+        let tuz = getZero tu
+        let taz = getZero ta
+        let tvz = getZero tv
+        let w = unbox<Cell<_>> taz
+        
+        match tu.Name, tv.Name with
+        | "Epsilon", "Epsilon" -> 
+            Triple(unbox<Epsilon> tuz, w, unbox<Epsilon> tvz) :> ITriple<_,_,_> |> box
+        | "Cell`1", "Epsilon" ->
+            Triple(unbox<Cell<_>> tuz, w, unbox<Epsilon> tvz) :> ITriple<_,_,_> |> box
+        | "Triple`3", "Epsilon" ->
+            Triple(castTrip<_,_,_> <| unbox<Triple<_,_,_>> tuz, w, unbox<Epsilon> tvz) :> ITriple<_,_,_> |> box
+        | "Epsilon", "Cell`1" -> 
+            Triple(unbox<Epsilon> tuz, w, unbox<Cell<_>> tvz) :> ITriple<_,_,_> |> box
+        | "Cell`1", "Cell`1" ->
+            Triple(unbox<Cell<_>> tuz, w, unbox<Cell<_>> tvz) :> ITriple<_,_,_> |> box
+        | "Triple`3", "Cell`1" ->
+            Triple(castTrip<_,_,_> <| unbox<Triple<_,_,_>> tuz, w, unbox<Cell<_>> tvz) :> ITriple<_,_,_> |> box
+        | "Epsilon", "Triple`3" -> 
+            Triple(unbox<Epsilon> tuz, w, castTrip<_,_,_> <| unbox<Triple<_,_,_>> tvz) :> ITriple<_,_,_> |> box
+        | "Cell`1", "Triple`3" ->
+            Triple(unbox<Cell<_>> tuz, w, castTrip<_,_,_> <| unbox<Triple<_,_,_>> tvz) :> ITriple<_,_,_> |> box
+        | "Triple`3", "Triple`3" ->
+            Triple(castTrip<_,_,_> <| unbox<Triple<_,_,_>> tuz, w, castTrip<_,_,_> <| unbox<Triple<_,_,_>> tvz) :> ITriple<_,_,_> |> box
+        | _ -> invalidArg "_type" "Invalid trace encoding type"
+    | _ -> invalidArg "_type" "Invalid trace encoding type"
+
+and castTrip<'u,'a,'v> (x:Triple<'u,obj,'v>) : Triple<'u,'a,'v> =
+    let x = x:> ITriple<'u,Cell<obj>,'v>
+    let casta = 
+        match x.A with
+        | NotExecuted -> Cell<'a>.NotExecuted
+        | Result y -> Cell<'a>.Result  (unbox<'a> y)
+    Triple<_,_,_>(x.U, casta, x.V)
+
+getZero typeof<Epsilon> |> unbox<Epsilon>
+
+let ppp<'u,'a,'v> x =
+    x //getZero typeof<Triple<'u,Cell<'a>,'v>> 
+    |> unbox<Triple<'u,obj,'v>>
+    |> castTrip<'u,'a,'v>
+
+getZero typeof<Triple<Epsilon,Cell<int>,Epsilon>> 
+|> unbox<Triple<Epsilon,obj,Epsilon>>
+|> castTrip<_,int,_>
+
+let lll<'u,'a,'v> =
+    getZero typeof<Triple<'u,Cell<'a>,'v>> 
+    |> ppp<'u,'a,'v>
+
+lll<Epsilon,Cell<int>,Epsilon>
+
+
+lll<Triple<Epsilon,Cell<int>,Epsilon>,Cell<int>,Epsilon>
+
+getZero typeof<Triple<Epsilon,Cell<int>,Epsilon>> 
+|> unbox<Triple<Epsilon,Cell<int>,Epsilon>>
+
+let zzz<'T> =
+    let _type = typeof<'T>
+    unbox<'T> (getZero _type) 
+
+zzz< Triple<Triple<Epsilon,Cell<string>,Epsilon>,Cell<int>,Epsilon> >
+
+//:?> Triple<Triple<Epsilon,Cell<string>,Epsilon>,Cell<int>,Epsilon>
+
+x.GenericParameterAttributes
+let getZero<'t> () =
+  let t = typeof<'t> ()
+    if t. then
+
 
 type ResumableBuilder() =
     member b.Zero() =
@@ -62,18 +156,21 @@ type ResumableBuilder() =
             resume = fun _ -> epsilon, NotExecuted
             //zero_previous = ()
             //zero_last = ()
+            zero = epsilon
         } 
     member b.Return(x:'t) =
         { 
             resume = fun _ -> epsilon, (Result x)
             //zero_previous = ()
             //zero_last = Unchecked.defaultof<'t>
+            zero = epsilon
         }
     member b.Delay(generator:unit->Resumable<'u,'a>) =
         { 
             resume = fun x -> generator().resume x
             //zero_previous = generator().zero_previous
             //zero_last = Unchecked.defaultof<'a>
+            zero = generator().zero
         }
     member b.ReturnFrom(x) = x
     member b.Bind<'u,'a,'v,'b, 'x,'y,'z, 'x2,'y2,'z2
@@ -86,6 +183,7 @@ type ResumableBuilder() =
         { 
             //zero_previous = (g f.zero_last).zero_previous
             //zero_last = (g f.zero_last).zero_last
+            zero = 
             resume = fun (X : ITriple<'u, Cell<'a>, 'v>) ->
                          match X.A with
                          | NotExecuted ->
@@ -218,4 +316,5 @@ let s3,_ = execute m s2
 let s4,_ = execute m s3
 let s5,_ = execute m s4
 let s6,rr = execute m s5
+
 
